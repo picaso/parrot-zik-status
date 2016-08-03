13dc7ea9b6349cb4e040d12f63a8a21612c041d5
jaumard <jimmy.aumard@gmail.com>
2016-08-03 09:13:45 +0200

    Add logout confirmation popup

--- ParrotStatus/views/footer/Footer.swift
+++ ParrotStatus/views/footer/Footer.swift
@@ -4,10 +4,31 @@ class Footer: NSView {
 
     @IBOutlet var footer: NSView!
     @IBOutlet weak var deviceName: NSTextField!
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         NSBundle.mainBundle().loadNibNamed("Footer", owner: self, topLevelObjects: nil)
         self.addSubview(footer)
+    }
+    
+    func dialogOKCancel(question: String, text: String) -> Bool {
+        let myPopup: NSAlert = NSAlert()
+        myPopup.messageText = question
+        myPopup.informativeText = text
+        myPopup.alertStyle = NSAlertStyle.WarningAlertStyle
+        myPopup.addButtonWithTitle("OK")
+        myPopup.addButtonWithTitle("Cancel")
+        let res = myPopup.runModal()
+        if res == NSAlertFirstButtonReturn {
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func shutDown(view: NSView) {
+        let answer = dialogOKCancel("Do you really want to quit ?", text: "")
+        if(answer) {
+            exit(0)
+        }
     }
 }
